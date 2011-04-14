c593ef6cbfaa1cdcfddaafe52cda44c6e9a34ce9
minos <minos@ubuntu>
2011-04-13 17:22:10 -0700

    Adjusted the display sequence of the prefix notation

--- expression.py
+++ expression.py
@@ -15,7 +15,7 @@ def parse_exp(exp):
     par_cnt = 0
     reright = releft = rechop = ""
     tree = node(exp)
-    for i in reversed(xrange(len(exp))):
+    for i in (xrange(len(exp))):
         ch = exp[i]
         if PAR_OPEN.find(ch) != -1:
             par_cnt += 1
@@ -28,7 +28,7 @@ def parse_exp(exp):
             tree.left = parse_exp(left)
             tree.right = parse_exp(right)
             return tree
-    for i in reversed(xrange(len(exp))):
+    for i in (xrange(len(exp))):
         ch = exp[i]
         if PAR_OPEN.find(ch) != -1:
             par_cnt += 1
@@ -47,36 +47,55 @@ def parse_exp(exp):
     return tree
 
 def disp(tree, depth, lr = ''):
-    print lr+"%d:" % depth, tree.opt, 
 
     if tree.opt == None:
-        print tree.data 
-        return 0
-    if tree.left.opt == None:
-        print tree.left.data,
+        return tree.data
     else:
-        left_label = lr + "L%d" % (depth + 1)
-        print left_label,
-    if tree.right.opt == None:
-        print tree.right.data
-    else:
-        right_label = lr + "R%d" % (depth + 1)
-        print right_label
-    if tree.left.opt != None:
-        disp(tree.left, depth + 1, lr + 'L')
-    if tree.right.opt != None:
-        disp(tree.right, depth + 1, lr + 'R')
+        right_label = disp(tree.right, depth + 1, lr + 'R')
+        left_label = disp(tree.left, depth + 1, lr + 'L')
+        if lr == '':
+            node_label = "F%d" % depth
+        else:
+            node_label = lr+"%d" % depth
+        print tree.opt, left_label + "  @@@@  " + right_label + "    =>    " + node_label
+        return node_label
+        
+
+    ## if tree.left.opt == None:
+    ##     print tree.left.data + "\t@@@@\t",
+    ## else:
+    ##     left_label = lr + "L%d" % (depth + 1) 
+    ##     print left_label + "\t@@@@\t", 
+    ## if tree.right.opt == None:
+    ##     print tree.right.data
+    ## else:
+    ##     right_label = lr + "R%d" % (depth + 1)
+    ##     print right_label
+
+    ## if tree.left.opt != None:
+    ##     disp(tree.left, depth + 1, lr + 'L')
+    ## if tree.right.opt != None:
+    ##     disp(tree.right, depth + 1, lr + 'R')
     return 0
     
 
 ## exp = "(2/(1+exp(-1000*x[2])))-1"
 ## exp = "(1/M[j])*(KSR[j]*LcePtrs[0]-(KSR[j]+KPR[j])*x[1]-CSS[0]*(B0[j]+B[j]*x[0])*sig[j]*(pow((fabs(x[2])),a[j]))-(F0[j]+F[j]*x[0])-KSR[j]*LSR0[j]+KPR[j]*LPR0[j])"
-exp = "GII[j]*(X[j]*L2nd[j]/LSR0[j]*(LcePtrs[0]-x[1]-LSR0[j])+(1-X[j])*L2nd[j]/LPR0[j]*(LcePtrs[0]-(LcePtrs[0]-x[1]-LSR0[j])-LSR0[j]-LPRN[j]))"
 exp = "(Ia_fiber[j]>0)*Ia_fiber[j]*(Ia_fiber[j]<100000)+(Ia_fiber[j]>100000)*100000"
 exp = "((a1+a2)+(b1+b2))*((c1+c2)+(d1+d2))"
 exp = "GII[j]*(X[j]*L2nd[j]/LSR0[j]*(LcePtrs[0]-x[1]-LSR0[j])+(1-X[j])*L2nd[j]/LPR0[j]*(LcePtrs[0]-(LcePtrs[0]-x[1]-LSR0[j])-LSR0[j]-LPRN[j]))"
 exp = "GI[j]*(LcePtrs[0]-x[1]-LSR0[j])"
 exp = "(2/(1+exp(-1000*x[2])))-1"
+exp = "GII[j]*(X[j]*L2nd[j]/LSR0[j]*(LcePtrs[0]-x[1]-LSR0[j])+(1-X[j])*L2nd[j]/LPR0[j]*(LcePtrs[0]-(LcePtrs[0]-x[1]-LSR0[j])-LSR0[j]-LPRN[j]))"
+exp = "GI[j]*(LcePtrs[0]-x[1]-LSR0[j])"
+exp = "GII[j]*(C0*Ia_fiber_R1+C1*(LcePtrs[0]-Ia_fiber_R1-C2))"
+exp = "(2/(1+exp_minus_1000_by_x_2))-1"
+exp = "(1/M[j])*(KSR[j]*(LcePtrs[0])-(KSR[j]+KPR[j])*x[1]-CSS[j]*(B0[j]+B[j]*x[0])*sig[j]*(abs_x2_pow_25)-(F0[j]+F[j]*x[0])-KSR[j]*LSR0[j]+KPR[j]*LPR0[j])"
+exp = "C3*(C4-(C5)*x[1]-CSS[j]*(B0[j]+B[j]*x[0])*sig[j]*(abs_x2_pow_25)-(F0[j]+F[j]*x[0])-C6+C7)"
+exp = "C3*(KSR[j]*(LcePtrs[0])-(C5)*x[1]-css_F0*(B0[j]+B[j]*x[0])*sig[j]*(abs_x2_pow_25)-(F0[j]+F[j]*x[0])-C6+C7)"
+exp = "C_REV_M*(KSR[j]*(LcePtrs[0])-(C_KSR_P_KPR)*x[1]-CSS[j]*(B0[j]+B[j]*x[0])*sig[j]*(abs_x2_pow_25)-(F0[j]+F[j]*x[0])-C_KSR_M_LSR0+C_KPR_M_LSR0)"
+exp = "gd_sqr/((gd_sqr)+C_60_SQR)"
+exp = "(Mingd-x[0])*(32'h40d6_3cdd)"
 tree = parse_exp(exp)
 lbl_cnt = 0
-disp(tree, 0)
+parsed_tree = disp(tree, 0)
