21e1bad48349731b7844d3612741f3c75a91a4b5
labuser <labuser@localhost.localdomain>
2012-08-29 18:32:57 -0700

    ADDED: + WaveGen designed using OOP, IoC, unittest, FP NOTES: * treat xxxGenerator.py as plugin * WaveGen.test module for unittesting the whole class * Each plugin contains its own unittest cases TODO: - SeqGenerator.py - Easy access from outside

---
+++ WaveGen/SeqGenerator.py
@@ -0,0 +1,18 @@
+'''
+Created on Aug 29, 2012
+
+@author: labuser
+'''
+
+class SeqGenerator(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        print("Constructed SeqObj!")
+        
